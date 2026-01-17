34127977eda184e1586c950d7caf51fb5dde3434
Nabil Anis <202440098+nabil-anis@users.noreply.github.com>
2026-01-18 02:06:16 +0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -35,11 +35,12 @@ export const ACHIEVEMENTS: Achievement[] = [
     date: '17–18 DECEMBER 2025',
     category: 'RESEARCH',
     images: [
-      'https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2340&auto=format&fit=crop',
-      'https://images.unsplash.com/photo-1532187863486-abf9d39d6618?q=80&w=2340&auto=format&fit=crop',
-      'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?q=80&w=2340&auto=format&fit=crop'
+      '/tAED.jpg',
+      '/ta1',
+      '/t2',
+      '/t4'
     ],
-    video: 'https://assets.mixkit.co/videos/preview/mixkit-doctor-checking-a-patients-lungs-with-a-stethoscope-40502-large.mp4'
+    video: '/t5'
   },
   {
     id: 'filmfest-2025',
